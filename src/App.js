c5865238fb1e7b3399ae45eb6abbbdec3b904e4a
arthuremment <artshiv13@gmail.com>
2023-10-25 21:01:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Main from './components/Main';
+import Die from './components/Die';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div>
+
+      <Main />
+         
+    </div>
+  );
+}
+
+export default App;
